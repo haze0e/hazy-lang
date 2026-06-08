49507c9c444760747fd41cfdb72912bb88b0e38a
Haze <chiteshjindal2008@gmail.com>
2026-06-08 12:28:38 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <any>
 #include <cstring>
 #include <fstream>
 #include <iostream>
@@ -5,70 +6,110 @@
 #include <string>
 #include <vector>
 
-std::string read_file_contents(const std::string &filename);
+enum class type {
+  END_OF_FILE,
+  LEFT_PAREN,
+  RIGHT_PAREN,
+};
 
-int main(int argc, char *argv[])
-{
-    // Disable output buffering
-    std::cout << std::unitbuf;
-    std::cerr << std::unitbuf;
-
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::cerr << "Logs from your program will appear here!" << std::endl;
-
-    if (argc < 3)
-    {
-        std::cerr << "Usage: ./your_program tokenize <filename>" << std::endl;
-        return 1;
-    }
-
-    const std::string command = argv[1];
-
-    if (command == "tokenize")
-    {
-        std::vector<std::string> tokens;
-        std::string file_contents = read_file_contents(argv[2]);
-
-        for (int i = 0; i < file_contents.length(); i++)
-        {
-            // some operation
-        }
-        tokens.push_back("EOF  null");
-        for (int i = 0; i < tokens.size(); i++)
-        {
-            std::cout << tokens[i] << std::endl;
-            // some operation
-        }
-
-        // TODO: Uncomment the code below to pass the first stage
-        //
-        // if (!file_contents.empty()) {
-        //     std::cerr << "Scanner not implemented" << std::endl;
-        //     return 1;
-        // }
-        // std::cout << "EOF  null" << std::endl; // Placeholder, replace this line when implementing the scanner
-    }
-    else
-    {
-        std::cerr << "Unknown command: " << command << std::endl;
-        return 1;
-    }
-
-    return 0;
+std::string to_string(type s) {
+  switch (s) {
+  case type::END_OF_FILE:
+    return "EOF";
+  case type::LEFT_PAREN:
+    return "LEFT_PAREN";
+  case type::RIGHT_PAREN:
+    return "RIGHT_PAREN";
+  default:
+    return "UNKNOWN";
+  }
 }
 
-std::string read_file_contents(const std::string &filename)
-{
-    std::ifstream file(filename);
-    if (!file.is_open())
-    {
-        std::cerr << "Error reading file: " << filename << std::endl;
-        std::exit(1);
+std::string read_file_contents(const std::string &filename);
+
+int main(int argc, char *argv[]) {
+  // Disable output buffering
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
+
+  // You can use print statements as follows for debugging, they'll be visible
+  // when running tests.
+  std::cerr << "Logs from your program will appear here!" << std::endl;
+
+  if (argc < 3) {
+    std::cerr << "Usage: ./your_program tokenize <filename>" << std::endl;
+    return 1;
+  }
+
+  const std::string command = argv[1];
+
+  if (command == "tokenize") {
+
+    class token {
+    private:
+    public:
+      type token_type;
+      std::string lexeme;
+      std::any literal;
+      token(type token_type, std::string lexeme, std::any literal = std::any{})
+          : token_type(token_type), lexeme(lexeme), literal(literal) {}
+    };
+
+    std::vector<token> tokens;
+
+    std::string file_contents = read_file_contents(argv[2]);
+
+    for (int i = 0; i < file_contents.length(); i++) {
+      switch (file_contents[i]) {
+      case '(':
+        tokens.push_back(token(type::LEFT_PAREN, "("));
+        break;
+      case ')':
+        tokens.push_back(token(type::RIGHT_PAREN, ")"));
+        break;
+
+      default:
+        break;
+      }
+      // some operation
+    }
+    tokens.push_back(token(type::END_OF_FILE, ""));
+    for (int i = 0; i < tokens.size(); i++) {
+      std::cout << to_string(tokens[i].token_type) + " " << tokens[i].lexeme
+                << " ";
+      if (tokens[i].literal.has_value()) {
+        if (auto str_val = std::any_cast<std::string>(&tokens[i].literal)) {
+          std::cout << *str_val << std::endl;
+        } else if (auto double_val =
+                       std::any_cast<double>(&tokens[i].literal)) {
+          std::cout << *double_val << std::endl;
+        } else {
+          std::cout << "unknown" << std::endl;
+        }
+      } else {
+        std::cout << "null" << std::endl;
+      }
+      // some operation
     }
 
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-    file.close();
+  } else {
+    std::cerr << "Unknown command: " << command << std::endl;
+    return 1;
+  }
 
-    return buffer.str();
+  return 0;
+}
+
+std::string read_file_contents(const std::string &filename) {
+  std::ifstream file(filename);
+  if (!file.is_open()) {
+    std::cerr << "Error reading file: " << filename << std::endl;
+    std::exit(1);
+  }
+
+  std::stringstream buffer;
+  buffer << file.rdbuf();
+  file.close();
+
+  return buffer.str();
 }
